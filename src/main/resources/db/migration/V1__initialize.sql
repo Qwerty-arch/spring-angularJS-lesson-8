bed77dc63c3f75785d9748dea0e9f711d1eceeb8
Qwerty <oshovsky.vlad@yandex.ru>
2021-03-27 15:02:26 +0800

    Commit 13.add tests

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -66,7 +66,6 @@ values
 ('chartreux cat', 40004),
 ('birman cat', 50000);
 
-
 create table orders (
     id                      bigserial primary key,
     owner_id                bigint references users (id),
@@ -88,3 +87,19 @@ create table order_items (
     updated_at              timestamp default current_timestamp
 );
 
+create table carts (
+    id                      UUID primary key,
+    price                   int
+);
+
+create table cart_items (
+    id                bigserial primary key,
+    cart_id           UUID references carts (id),
+    product_id        bigint references products (id),
+    title             varchar(255),
+    quantity          int,
+    price_per_product int,
+    price             int,
+    created_at        timestamp default current_timestamp,
+    updated_at        timestamp default current_timestamp
+);
--- src/test/java/com/oshovskii/market/tests/MarketApplicationTests.java
+++ src/test/java/com/oshovskii/market/tests/MarketApplicationTests.java
@@ -1,4 +1,4 @@
-package com.oshovskii.market;
+package com.oshovskii.market.tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
